51a0dc92ead8c39f41978fbdc7673f737d7f3b52
danialmkj <danialmalekij@yahoo.com>
2022-05-23 16:19:28 +0430

    New Asset Directions
    Change Layout Logo with change screen size

--- lib/src/helpers/responsiveness.dart
+++ lib/src/helpers/responsiveness.dart
@@ -15,7 +15,7 @@ class ResponsiveWidget extends StatelessWidget {
 
   //TODO: Define Screen Size
   static bool isSmallScreen(BuildContext context) =>
-  MediaQuery.of(context).size.width < smallScreenSize;
+  MediaQuery.of(context).size.width < mediumScreenSize;
 
 
   static bool isMediumScreen(BuildContext context)=>
--- lib/src/widgets/top_nav_bar.dart
+++ lib/src/widgets/top_nav_bar.dart
@@ -1,13 +1,20 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_web_test/src/helpers/responsiveness.dart';
 
 AppBar topNavigationAppBar(BuildContext context , GlobalKey<ScaffoldState> key) =>
 
   AppBar(
     elevation: 0.0,
-    backgroundColor:Colors.white,
-    title: const Text('<< Flutter Web Panel >>' , style: TextStyle(color: Colors.black),),
-    centerTitle: true,
+    leading: !ResponsiveWidget.isSmallScreen(context) ? 
+    Row(children: [
+      Container(
+        padding: EdgeInsets.only(left: 14.0),
+        child: Image.asset("assets/icons/logo.png" , width: 28.0,),
+      )
+    ],) : IconButton(onPressed: (){
+      key.currentState!.openDrawer();
+    }, icon: Icon(Icons.menu)) ,
   );
 
   //TODO: why we use Entire Value ?
